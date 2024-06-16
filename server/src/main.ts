320fcc96d30b6320f46d1a138ab241b7be55f908
CodeArmer <geanmotas@gmail.com>
2024-06-16 03:27:54 -0400

    chore: Update routes to include '/api' prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,12 +5,12 @@ import { courses } from './routes/courses.ts'
 
 export const ulid = monotonicFactory()
 export const db = await Deno.openKv()
-const app = new Hono().basePath('/api')
+const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
-app.route('/courses/', courses)
-app.route('/auth/', auth)
+app.route('/api/courses/', courses)
+app.route('/api/auth/', auth)
 
 export default app

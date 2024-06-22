1c82a8cb73fb361db8ed5cfd488bee10b359c983
CodeArmer <geanmotas@gmail.com>
2024-06-22 05:27:20 -0400

    refactor: Update auth route to use initialOptions for password recovery

--- Math-Wizard-Client/services/auth.js
+++ Math-Wizard-Client/services/auth.js
@@ -1,10 +1,13 @@
+const initialOptions = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}
 const url = 'https://codearmer-math-wizard-64.deno.dev/api/auth/'
 export async function login ({ username, password }) {
   const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+    ...initialOptions,
     body: JSON.stringify({ username, password })
   }
   const response = await fetch(url, options)
@@ -13,10 +16,7 @@ export async function login ({ username, password }) {
 }
 export async function register ({ email, password, username }) {
   const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+    ...initialOptions,
     body: JSON.stringify({ email, password, username })
   }
   const response = await fetch(url, options)
@@ -25,10 +25,7 @@ export async function register ({ email, password, username }) {
 }
 export async function loginWithEmail ({ email, verificationCode }) {
   const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+    ...initialOptions,
     body: JSON.stringify({ email, verificationCode })
   }
   const response = await fetch(url, options)
@@ -37,10 +34,7 @@ export async function loginWithEmail ({ email, verificationCode }) {
 
 export async function sendRecoveryEmail ({ email }) {
   const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
+    ...initialOptions,
     body: JSON.stringify({ email })
   }
   const response = await fetch(url + 'recovery', options)
@@ -49,10 +43,8 @@ export async function sendRecoveryEmail ({ email }) {
 
 export async function updatePassword ({ token, password }) {
   const options = {
+    ...initialOptions,
     method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json'
-    },
     body: JSON.stringify({ token, password })
   }
   const response = await fetch(url + 'recovery', options)

502ccd4278bbd1781d479f5de73a943e3454645f
CodeArmer <geanmotas@gmail.com>
2024-07-12 05:43:46 -0400

    refactor: Add CORS middleware to API routes

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,5 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import { monotonicFactory } from 'ulid'
 import { auth } from './routes/auth.ts'
 import { courses } from './routes/courses.ts'
@@ -6,7 +7,7 @@ import { courses } from './routes/courses.ts'
 export const ulid = monotonicFactory()
 export const db = await Deno.openKv()
 const app = new Hono()
-
+app.use('/api/*', cors())
 app.get('/', (c) => {
   return c.text('Welcome to the Math Wizard API!')
 })
